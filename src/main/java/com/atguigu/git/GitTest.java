295572f51b3cfb2d0d34211348196b1c8894a045
正正正 <1247461063@qq.com>
2021-08-15 17:37:42 +0800

    my first commit by idea hot-fix

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -5,5 +5,7 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("hello git01!");
         System.out.println("hello git02!");
+        System.out.println("hello git03!");
+        System.out.println("hello git04!");
     }
 }
