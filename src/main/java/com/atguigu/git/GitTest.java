7c1407c7237f559b0b76041717b20b77337ddd5b
正正正 <1247461063@qq.com>
2021-08-15 17:31:16 +0800

    my third commit by idea

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("hello git01!");
+        System.out.println("hello git02!");
     }
 }
