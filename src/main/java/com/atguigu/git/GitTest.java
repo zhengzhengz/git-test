de02975ab7b9be4c10a185d8ebc8b46ebec148b9
zhengzhengz <88923033+zhengzhengz@users.noreply.github.com>
2021-08-15 18:27:18 +0800

    by github

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello git04!");
         System.out.println("hello git05 by hot-fix!");
         System.out.println("hello git05 by master!");
+        System.out.println("hello git06 by github!");
     }
 }
